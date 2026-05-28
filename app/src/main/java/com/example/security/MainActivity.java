5f2a7cff75ef78cddfdeb5bbf10c616a90968c58
Misha <axxuetam@gmail.com>
2026-05-28 11:22:58 +0500

    MainActivity Казаков

--- app/src/main/java/com/example/security/MainActivity.java
+++ app/src/main/java/com/example/security/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.security;
 
+import android.content.Context;
+import android.hardware.biometrics.BiometricPrompt;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -10,15 +15,37 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Context context;
+    BiometricsHelper biometricsHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        context = this;
+        Button btnLogin = new BiometricsHelper(this, biometricsHelper.callback);
+
+        btnLogin.setOnClickListener(v -> {
+            biometricsHelper.show();
         });
+    }
+
+    BiometricPrompt.AuthenticationCallback callback = new BiometricPrompt.AuthenticationCallback() {
+        @Override
+        public void onAuthenticationError(int errorCode, CharSequence errString) {
+            super.onAuthenticationError(errorCode, errString);
+        }
+
+        @Override
+        public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
+            super.onAuthenticationSucceeded(result);
+            Toast.makeText(context, "Авторизация пройдена", Toast.LENGTH_SHORT)
+        }
+
+        @Override
+        public void onAuthenticationFailed() {
+            super.onAuthenticationFailed();
+        }
     }
 }
